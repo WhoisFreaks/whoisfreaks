b822eaabb40ee7a0c6783ba6ae1cb3263803745e
Usama015 <usama@jfreaks.com>
2023-11-02 17:34:43 +0500

    used all flags as pointers.

--- main.go
+++ main.go
@@ -29,36 +29,18 @@ func main() {
 	mini := flag.Bool("mini", false, "used for returning mini whois Responses that contains fewer fields. It is only useful with reverse whois query.")
 	bulk := flag.Bool("bulk", false, "for specifying a bulk query of whois service.")
 	sug := flag.Bool("sug", false, "for activating the suggestions.")
-	count := flag.String("count", "", "for specifying the number of suggestions.")
 	chain := flag.Bool("chain", false, "for mention to Extract the chain certificate")
 	raw := flag.Bool("raw", false, "for mention to Extract the raw certificate")
-
-	var domain string
-	flag.StringVar(&domain, "domain", "", "Domain Name for which you need to get whois Information. Only for live and historical whois query.")
-
-	var domains string
-	flag.StringVar(&domains, "domains", "", "Domain Names for bulk query. Only for live Whois and Domain Availability query.")
-
-	var company_name string
-	flag.StringVar(&company_name, "company", "", "Company Name for which you need to get whois Information. only with reverse whois query.")
-
-	var keyword string
-	flag.StringVar(&keyword, "keyword", "", "Keyword for which you need to get whois Information. only with reverse whois query.")
-
-	var owner_name string
-	flag.StringVar(&owner_name, "owner", "", "Owner Name for which you need to get whois Information. only with reverse whois query.")
-
-	var email string
-	flag.StringVar(&email, "email", "", "Email for which you need to get whois Information. only with reverse whois query.")
-
-	var dnsType string
-	flag.StringVar(&dnsType, "dnsType", "", "Dns Record type for which you need to get DNS Information. use 'all' for retrieving all types of records.")
-
-	var value string
-	flag.StringVar(&value, "value", "", "Value of record type for which you need to get DNS Information. e.g. 8.8.8.8 for ARecord")
-
-	var page string
-	flag.StringVar(&page, "page", "", "Required Page Number. Each page contains 100 records.")
+	count := flag.String("count", "", "for specifying the number of suggestions.")
+	domain := flag.String("domain", "", "Domain Name for which you need to get whois Information. Only for live and historical whois query.")
+	domains := flag.String("domains", "", "Domain Names for bulk query. Only for live Whois and Domain Availability query.")
+	company_name := flag.String("company", "", "Company Name for which you need to get whois Information. only with reverse whois query.")
+	keyword := flag.String("keyword", "", "Keyword for which you need to get whois Information. only with reverse whois query.")
+	owner_name := flag.String("owner", "", "Owner Name for which you need to get whois Information. only with reverse whois query.")
+	email := flag.String("email", "", "Email for which you need to get whois Information. only with reverse whois query.")
+	dnsType := flag.String("dnsType", "", "Dns Record type for which you need to get DNS Information. use 'all' for retrieving all types of records.")
+	value := flag.String("value", "", "Value of record type for which you need to get DNS Information. e.g. 8.8.8.8 for ARecord")
+	page := flag.String("page", "", "Required Page Number. Each page contains 100 records.")
 
 	flag.Parse()
 
@@ -71,14 +53,14 @@ func main() {
 	if *whoisPtr {
 		if *livePtr {
 			if *bulk {
-				domainInfo, errorInfo := whois.GetBulkLiveResponse(strings.Split(domains, ","), apiKey)
+				domainInfo, errorInfo := whois.GetBulkLiveResponse(strings.Split(*domains, ","), apiKey)
 				if errorInfo != nil {
 					utility.PrintError(errorInfo)
 					return
 				}
 				utility.PrintInfo(domainInfo)
 			} else {
-				domainInfo, errorInfo := whois.GetLiveResponse(domain, apiKey)
+				domainInfo, errorInfo := whois.GetLiveResponse(*domain, apiKey)
 				if errorInfo != nil {
 					utility.PrintError(errorInfo)
 					return
@@ -86,7 +68,7 @@ func main() {
 				utility.PrintInfo(domainInfo)
 			}
 		} else if *historicalPtr {
-			domainInfo, errorInfo := whois.GetHistoricalResponse(domain, apiKey)
+			domainInfo, errorInfo := whois.GetHistoricalResponse(*domain, apiKey)
 			if errorInfo != nil {
 				utility.PrintError(errorInfo)
 				return
@@ -94,14 +76,14 @@ func main() {
 			utility.PrintInfo(domainInfo)
 		} else if *reversePtr {
 			if *mini {
-				domainInfo, errorInfo := whois.GetReverseMiniResponse(keyword, email, company_name, owner_name, apiKey, page)
+				domainInfo, errorInfo := whois.GetReverseMiniResponse(*keyword, *email, *company_name, *owner_name, apiKey, *page)
 				if errorInfo != nil {
 					utility.PrintError(errorInfo)
 					return
 				}
 				utility.PrintInfo(domainInfo)
 			} else {
-				domainInfo, errorInfo := whois.GetReverseResponse(keyword, email, company_name, owner_name, apiKey, page)
+				domainInfo, errorInfo := whois.GetReverseResponse(*keyword, *email, *company_name, *owner_name, apiKey, *page)
 				if errorInfo != nil {
 					utility.PrintError(errorInfo)
 					return
@@ -111,21 +93,21 @@ func main() {
 		}
 	} else if *dnsPtr {
 		if *livePtr {
-			dnsInfo, errorInfo := dns.GetLiveResponse(dnsType, domain, apiKey)
+			dnsInfo, errorInfo := dns.GetLiveResponse(*dnsType, *domain, apiKey)
 			if errorInfo != nil {
 				utility.PrintError(errorInfo)
 				return
 			}
 			utility.PrintInfo(dnsInfo)
 		} else if *historicalPtr {
-			historicalDnsInfo, errorInfo := dns.GetHistoricalResponse(dnsType, domain, page, apiKey)
+			historicalDnsInfo, errorInfo := dns.GetHistoricalResponse(*dnsType, *domain, *page, apiKey)
 			if errorInfo != nil {
 				utility.PrintError(errorInfo)
 				return
 			}
 			utility.PrintInfo(historicalDnsInfo)
 		} else if *reversePtr {
-			reverseDnsInfo, errorInfo := dns.GetReverseResponse(dnsType, value, page, apiKey)
+			reverseDnsInfo, errorInfo := dns.GetReverseResponse(*dnsType, *value, *page, apiKey)
 			if errorInfo != nil {
 				utility.PrintError(errorInfo)
 				return
@@ -134,7 +116,7 @@ func main() {
 		}
 	} else if *domainAvailabilityPtr {
 		if *bulk {
-			bulkDomainavailabilityInfo, errorInfo := domainavailability.Bulk(strings.Split(domains, ","), apiKey)
+			bulkDomainavailabilityInfo, errorInfo := domainavailability.Bulk(strings.Split(*domains, ","), apiKey)
 			if errorInfo != nil {
 				utility.PrintError(errorInfo)
 				return
@@ -142,14 +124,14 @@ func main() {
 			utility.PrintInfo(bulkDomainavailabilityInfo)
 		} else {
 			if *sug {
-				domainavailabilityInfo, errorInfo := domainavailability.CheckAndSuggest(domain, apiKey, *sug, *count)
+				domainavailabilityInfo, errorInfo := domainavailability.CheckAndSuggest(*domain, apiKey, *sug, *count)
 				if errorInfo != nil {
 					utility.PrintError(errorInfo)
 					return
 				}
 				utility.PrintInfo(domainavailabilityInfo)
 			} else {
-				bulkDomainavailabilityInfo, errorInfo := domainavailability.Check(domain, apiKey)
+				bulkDomainavailabilityInfo, errorInfo := domainavailability.Check(*domain, apiKey)
 				if errorInfo != nil {
 					utility.PrintError(errorInfo)
 					return
@@ -159,7 +141,7 @@ func main() {
 		}
 	} else if *sslPtr {
 		if *livePtr {
-			sslInfo, errorInfo := ssl.GetLiveResponse(domain, apiKey, *chain, *raw)
+			sslInfo, errorInfo := ssl.GetLiveResponse(*domain, apiKey, *chain, *raw)
 			if errorInfo != nil {
 				utility.PrintError(errorInfo)
 				return
